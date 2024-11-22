61c681c0516555829bc670ffe33963e87ad12b78
Davo11030 <rdfh1103@gmail.com>
2024-11-22 04:30:34 -0800

    Navegacion desplazante

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -1,4 +1,36 @@
 //debera de declarase la navegacion, anexo ejemplo
+<div id="contentContainer" class="style-scope tp-yt-app-drawer" position="left" style="transition-duration: 200ms;" swipe-open="" opened="">
+  
+  <div id="guide-wrapper" class="style-scope ytd-app">
+    <div id="guide-spacer" class="style-scope ytd-app"></div>
+    <div id="guide-content" class="style-scope ytd-app">
+      <div id="header" class="style-scope ytd-app">
+        
+        <yt-icon-button id="guide-button" pressed="true" toggleable="true" class="style-scope ytd-app" aria-label="Guía" role="button">
+            <!--css-build:shady-->
+            <!--css-build:shady-->
+            <button id="button" class="style-scope yt-icon-button" aria-pressed="true">
+          <yt-icon id="guide-icon" icon="yt-icons:menu" class="style-scope ytd-app">
+            <!--css-build:shady-->
+            <!--css-build:shady-->
+            <span class="yt-icon-shape style-scope yt-icon yt-spec-icon-shape">
+                <div style="width: 100%; height: 100%; display: block; fill: currentcolor;"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;"><path d="M21 6H3V5h18v1zm0 5H3v1h18v-1zm0 6H3v1h18v-1z"></path></svg></div></span></yt-icon>
+        </button>
+        <yt-interaction id="interaction" class="circular style-scope yt-icon-button">
+            <!--css-build:shady--><!--css-build:shady-->
+            <div class="stroke style-scope yt-interaction"></div><div class="fill style-scope yt-interaction"></div>
+            </yt-interaction>
+            </yt-icon-button>
+        
+      </div>
+      
+    </div>
+  </div>
+
+</div>
+
+
+
 import { faFacebookF, faGooglePlus, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
